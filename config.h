48847caa46e1f3539b1579ef2f77e08c0bc05b15
Daniel Zenke <danielzenke94@gmail.com>
2025-06-20 23:15:49 +0200

    feat: add cpu usage block

--- config.h
+++ config.h
@@ -18,7 +18,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)                \
-    X("", "bar-keyboard", 0, 6)  \
+    X("", "bar-keyboard", 0, 7)  \
+    X("", "bar-cpu", 30, 6)      \
     X("", "bar-temp", 60, 5)     \
     X("", "bar-mic", 0, 4)       \
     X("", "bar-audio", 0, 3)     \
